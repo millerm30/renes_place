f1d073d5c0d1b46171b20465b0423660424f9735
Michael Miller <millergm30@gmail.com>
2022-08-28 12:59:11 -0230

    start your engines...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+  integrations: [tailwind(), react()],
 });
